07ced7a92d913c1629d33d6be2bfd776face5bdc
Vaidehi1423 <76743632+Vaidehi1423@users.noreply.github.com>
2021-02-28 18:01:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+// $(document).on('click', '.click-me', function(e){
+//     doSomething.call(this, e);
+//   });
+// })(jQuery);
+
+// function insertHTML(str){
+// var s = document.getElementsByTagName('script'), lastScript = s[s.length-1];
+// lastScript.insertAdjacentHTML("beforebegin", str);
+// }
+
+// function doSomething(event){
+// console.log(this.id); // this will be the clicked element
+// }
+// $(document).on('click', '.button1', function(e){
+//     doSomething.call(this, e);
+//   });
+// })(jQuery);
+
+// function insertHTML(str){
+// var s = document.getElementsByTagName('script'), lastScript = s[s.length-1];
+// lastScript.insertAdjacentHTML("beforebegin", str);
+// }
+
+// function doSomething(event){
+// console.log(this.id); // this will be the clicked element
+// }
+// var .right = document.createElement('span');
+// e.innerHTML = htmldata;
+// element.appendChild(e);
+//         function doSomething () {
+//             var x = document.createElement("INPUT");
+//             x.setAttribute("type", "file");
+//             x.setAttribute("id", "file");
+//             document.body.appendChild(x);
+//             x.onchange = function () {
+//                 hello();
+//             };
+//             btn.appendChild(t);
+//             document.body.appendChild(btn);
+//         }
+//         function hello() {
+//             window.alert("hello!");
+//         }
+    
